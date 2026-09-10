87a35429e2232a0a15171005d0ca9af4b14b3d14
Rahul2271 <rraahhuullch@gmail.com>
2026-09-10 16:43:24 +0530

    s

--- components/Header.tsx
+++ components/Header.tsx
@@ -42,7 +42,7 @@ export default function Header() {
             <svg width="11" height="11" viewBox="0 0 24 24" fill="currentColor" className="shrink-0">
               <path d="M6.6 10.8c1.4 2.8 3.8 5.1 6.6 6.6l2.2-2.2c.3-.3.7-.4 1-.2 1.1.4 2.3.6 3.6.6.6 0 1 .4 1 1V20c0 .6-.4 1-1 1C10.9 21 3 13.1 3 3c0-.6.4-1 1-1h3.5c.6 0 1 .4 1 1 0 1.3.2 2.5.6 3.6.1.4 0 .8-.2 1L6.6 10.8z" />
             </svg>
-            <span className="hidden sm:inline">{contact.phoneSecondary}</span>
+            <span className=" sm:inline">{contact.phoneSecondary}</span>
           </a>
           <button
             onClick={() => handleNavClick("booking")}
